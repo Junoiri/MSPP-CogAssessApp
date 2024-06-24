93687b70a6a7f053266367444655a2adc56cdafe
Polax2 <276087@student.pwr.edu.pl>
2024-06-24 23:45:12 +0200

    Game dto plus firebase functionality

--- app/src/main/java/firebase/GameDto.kt
+++ app/src/main/java/firebase/GameDto.kt
@@ -1,14 +1,15 @@
-package firebase
+package com.example.mspp_cogassessapp.firebase
 
-import java.sql.Date
+import java.util.Date
 import java.sql.Time
 
 data class GameDto(
     var email: String = "",
-    var date: Date=Date(System.currentTimeMillis()),
-    var responseTime: Time,
-    val totalTime: Time,
-    val accuracy: Double,
-){
-    constructor() : this("", Date(System.currentTimeMillis()), Time(0), Time(0), 0.0)
+    var title: String = "",
+    var date: Date = Date(System.currentTimeMillis()),
+    var responseTime: Time = Time(0),
+    var totalTime: Time = Time(0),
+    var accuracy: Double = 0.0
+) {
+    constructor() : this("", "", Date(System.currentTimeMillis()), Time(0), Time(0), 0.0)
 }
