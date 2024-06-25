87d2832df083325c9ffaea54efea50b3e3048a30
Junoiri <martasadlowska77@gmail.com>
2024-06-25 10:46:52 +0200

    Completed Namik game

--- app/src/main/java/com/example/mspp_cogassessapp/composable/games/play/NamikPlayScreen.kt
+++ app/src/main/java/com/example/mspp_cogassessapp/composable/games/play/NamikPlayScreen.kt
@@ -26,9 +26,14 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.mspp_cogassessapp.R
 import kotlinx.coroutines.delay
+import android.widget.Toast
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalContext
+
 
 @Composable
 fun NamikPlayScreen(navController: NavController) {
+    val context = LocalContext.current  // Get the context for the toast message
     val pressStartFontFamily = FontFamily(Font(R.font.press_start))
     var showAnswers by remember { mutableStateOf(false) }
     val blueColor = colorResource(id = R.color.blue)
@@ -36,17 +41,50 @@ fun NamikPlayScreen(navController: NavController) {
     var loadingColor by remember { mutableStateOf(blueColor) }
     var isPaused by remember { mutableStateOf(false) }
     var showDialog by remember { mutableStateOf(false) }
+    var showResultDialog by remember { mutableStateOf(false) }
     var counter by remember { mutableStateOf(4) }
+    var cycleCounter by remember { mutableStateOf(0) }  // Cycle counter
+    var correctAnswers by remember { mutableStateOf(0) }  // Correct answers counter
+    var totalQuestions by remember { mutableStateOf(0) }  // Total questions counter
+    var sequence by remember { mutableStateOf<List<Int>>(emptyList()) } // Sequence state
+    var answerOptions by remember { mutableStateOf<List<List<Int>>>(emptyList()) } // Answer options state
+    var selectedOption by remember { mutableStateOf<List<Int>?>(null) } // Selected option state
+
+    LaunchedEffect(cycleCounter) {
+        if (cycleCounter > 0 && cycleCounter % 10 == 0) {
+            isPaused = true
+            showResultDialog = true
+        }
+    }
 
     LaunchedEffect(Unit) {
-        while (counter > 0) {
-            delay(1000)
-            if (!isPaused) {
-                counter--
+        sequence = generateSequence()
+        answerOptions = generateAnswerOptions(sequence)
+        while (true) {
+            while (counter > 0) {
+                delay(1000)
+                if (!isPaused) {
+                    counter--
+                }
             }
+            loadingColor = if (loadingColor == blueColor) redColor else blueColor
+            showAnswers = !showAnswers
+
+            if (showAnswers) {
+                // Displaying answers, do nothing special here
+            } else {
+                // After the answer phase, increment cycleCounter and generate new sequence and answers
+                cycleCounter++  // Increment cycle counter only when an answer cycle ends
+                if (cycleCounter > 0 && cycleCounter % 10 == 0) {
+                    isPaused = true
+                    showResultDialog = true
+                }
+                sequence = generateSequence()
+                answerOptions = generateAnswerOptions(sequence)
+                selectedOption = null // Reset selected option
+            }
+            counter = 4
         }
-        loadingColor = redColor
-        showAnswers = true
     }
 
     val loadingProgress by animateFloatAsState(
@@ -98,28 +136,43 @@ fun NamikPlayScreen(navController: NavController) {
                 verticalArrangement = Arrangement.Top // Align elements to the top
             ) {
                 if (showAnswers) {
-                    DisplayAnswerGrid()
+                    DisplayAnswerGrid(answerOptions, selectedOption) { option ->
+                        // Handle answer click
+                        totalQuestions++
+                        if (option == sequence) {
+                            correctAnswers++
+                        }
+                        selectedOption = option
+                    }
                 } else {
-                    DisplaySequence()
+                    DisplaySequence(sequence)
                 }
             }
             Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .align(Alignment.BottomCenter) // Align to the bottom
+                    .fillMaxSize()
+                    .padding(top = 150.dp), // Move everything down by 150 dp
+                contentAlignment = Alignment.Center // Align to the center
             ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxWidth()
+                Box(
+                    contentAlignment = Alignment.Center,
                 ) {
+                    CircularProgressIndicator(
+                        progress = 1f - (counter / 4f),
+                        color = loadingColor,
+                        modifier = Modifier
+                            .size(100.dp) // Set the size of the loading circle
+                            .padding(top = 50.dp), // Add top margin
+                        strokeWidth = 8.dp // Make the loading circle thicker
+                    )
                     Text(
                         text = counter.toString(),
                         fontFamily = pressStartFontFamily,
                         color = Color.White,
                         fontSize = 32.sp,
-                        modifier = Modifier.padding(bottom = 8.dp)
+                        modifier = Modifier.align(Alignment.Center)
+                            .padding(top = 100.dp)
                     )
-                    LoadingStrip(loadingColor, loadingProgress)
                 }
             }
             if (showDialog) {
@@ -128,22 +181,20 @@ fun NamikPlayScreen(navController: NavController) {
                     onQuit = { navController.popBackStack() }
                 )
             }
+            if (showResultDialog) {
+                ResultDialog(
+                    correctAnswers = correctAnswers,
+                    totalQuestions = totalQuestions,
+                    onContinue = { isPaused = false; showResultDialog = false },
+                    onQuit = { navController.popBackStack() }
+                )
+            }
         }
     }
 }
 
 @Composable
-fun DisplaySequence() {
-    val icons = listOf(
-        R.drawable.namik_ic_1,
-        R.drawable.namik_ic_2,
-        R.drawable.namik_ic_3,
-        R.drawable.namik_ic_4,
-        R.drawable.namik_ic_5
-    ).shuffled()
-
-    val sequence = remember { icons.take(4) }
-
+fun DisplaySequence(sequence: List<Int>) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
@@ -175,40 +226,7 @@ fun DisplaySequence() {
 }
 
 @Composable
-fun LoadingStrip(color: Color, progress: Float) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(24.dp) // Thicker height
-            .background(color) // White color for the loading strip
-            .fillMaxWidth(fraction = progress) // Adjust width according to progress
-    )
-}
-
-@Composable
-fun DisplayAnswerGrid() {
-    val icons = listOf(
-        R.drawable.namik_ic_1,
-        R.drawable.namik_ic_2,
-        R.drawable.namik_ic_3,
-        R.drawable.namik_ic_4,
-        R.drawable.namik_ic_5
-    ).shuffled()
-
-    val sequence = icons.take(4)
-    val otherOptions = mutableListOf<List<Int>>()
-
-    // Generate three other random sequences
-    for (i in 0 until 3) {
-        val option = icons.shuffled().take(4)
-        if (option != sequence && option !in otherOptions) {
-            otherOptions.add(option)
-        }
-    }
-
-    // Add the correct sequence to the options and shuffle them
-    val options = (otherOptions + listOf(sequence)).shuffled()
-
+fun DisplayAnswerGrid(options: List<List<Int>>, selectedOption: List<Int>?, onOptionClick: (List<Int>) -> Unit) {
     Column(
         modifier = Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -221,9 +239,12 @@ fun DisplayAnswerGrid() {
                 row.forEach { option ->
                     Card(
                         shape = RoundedCornerShape(8.dp),
+                        backgroundColor = if (selectedOption == option) Color.White else Color.White.copy(alpha = 0.8f),
                         modifier = Modifier
                             .weight(1f)
                             .aspectRatio(1f)
+                            .clickable { onOptionClick(option) }
+                            .shadow(if (selectedOption == option) 8.dp else 0.dp, RoundedCornerShape(8.dp))
                     ) {
                         Row(
                             modifier = Modifier
@@ -268,6 +289,62 @@ fun PauseDialog(onResume: () -> Unit, onQuit: () -> Unit) {
             }
         }
     )
+}
+
+@Composable
+fun ResultDialog(correctAnswers: Int, totalQuestions: Int, onContinue: () -> Unit, onQuit: () -> Unit) {
+    val pressStartFontFamily = FontFamily(Font(R.font.press_start))
+
+    AlertDialog(
+        onDismissRequest = { /* Do nothing */ },
+        title = { Text(text = "Results", fontFamily = pressStartFontFamily) },
+        text = { Text("You got $correctAnswers out of $totalQuestions correct.", fontFamily = pressStartFontFamily) },
+        confirmButton = {
+            Button(onClick = onContinue) {
+                Text("Continue", fontFamily = pressStartFontFamily)
+            }
+        },
+        dismissButton = {
+            Button(onClick = onQuit) {
+                Text("Quit", fontFamily = pressStartFontFamily)
+            }
+        }
+    )
+}
+
+fun generateSequence(): List<Int> {
+    val icons = listOf(
+        R.drawable.namik_ic_1,
+        R.drawable.namik_ic_2,
+        R.drawable.namik_ic_3,
+        R.drawable.namik_ic_4,
+        R.drawable.namik_ic_5
+    ).shuffled()
+
+    return icons.take(4)
+}
+
+fun generateAnswerOptions(correctSequence: List<Int>): List<List<Int>> {
+    val icons = listOf(
+        R.drawable.namik_ic_1,
+        R.drawable.namik_ic_2,
+        R.drawable.namik_ic_3,
+        R.drawable.namik_ic_4,
+        R.drawable.namik_ic_5
+    )
+
+    val otherOptions = mutableListOf<List<Int>>()
+
+    // Generate three other random sequences
+    for (i in 0 until 3) {
+        val option = icons.shuffled().take(4)
+        if (option != correctSequence && option !in otherOptions) {
+            otherOptions.add(option)
+        }
+    }
+
+    // Add the correct sequence to the options and shuffle them
+    return (otherOptions + listOf(correctSequence)).shuffled()
 }
 
 @Preview(showBackground = true)
